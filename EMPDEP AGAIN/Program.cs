d7e3760cf251c3cb2aee351b41d674e0bda683cc
rebeccapatek <rebeccapatek@gmail.com>
2020-03-24 10:10:12 -0500

    added legal dept

--- EMPDEP AGAIN/Program.cs
+++ EMPDEP AGAIN/Program.cs
@@ -9,50 +9,60 @@ namespace DepartmentsEmployeesConsole
         static void Main(string[] args)
         {
             var repo = new EmployeeRepository();
-            //var employees = repo.GetAllEmployees();
+            var employees = repo.GetAllEmployees();
 
-            //foreach (var employee in employees)
-            //{
-            //    Console.WriteLine($"{employee.FirstName} {employee.LastName} is in {employee.Department.DeptName}");
-            //}
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} is in {employee.Department.DeptName}");
+            }
 
 
-            //Console.WriteLine("Let's get an employee with the ID 2");
+            Console.WriteLine("Let's get an employee with the ID 2");
 
-            //var employeeWithId2 = repo.GetEmployeeById(2);
+            var employeeWithId2 = repo.GetEmployeeById(2);
 
-            //Console.WriteLine($"Employee with Id 2 is {employeeWithId2.FirstName} {employeeWithId2.LastName}");
+            Console.WriteLine($"Employee with Id 2 is {employeeWithId2.FirstName} {employeeWithId2.LastName}");
 
-            //Console.WriteLine("First Name");
-            //var firstName = Console.ReadLine();
+            Console.WriteLine("First Name");
+            var firstName = Console.ReadLine();
 
-            //Console.WriteLine("Last Name");
-            //var lastName = Console.ReadLine();
+            Console.WriteLine("Last Name");
+            var lastName = Console.ReadLine();
 
-            //var newEmployee = new Employee()
-            //{
-              //  FirstName = firstName,
-                //LastName = lastName,
-               // DepartmentId = 2
-            //};
+            var newEmployee = new Employee()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                DepartmentId = 2
+            };
 
-            //repo.CreateNewEmployee(newEmployee);
+            repo.CreateNewEmployee(newEmployee);
             
-            //var deprepo = new DepartmentRepository();
-            //var departments = deprepo.GetAllDepartmentss();
-            //foreach (var dep in departments)
-            //{
-               // Console.WriteLine($"{dep.DeptName}");
-            //}
-            //Console.WriteLine("Lets find the Deparment with Id 1");
-            //var deptWithId1 = deprepo.GetDepartmentById(1);
-            //Console.WriteLine($"Department with Id 1 is {deptWithId1.DeptName}");
+            var deprepo = new DepartmentRepository();
+            var departments = deprepo.GetAllDepartmentss();
+            foreach (var dep in departments)
+            {
+                Console.WriteLine($"{dep.DeptName}");
+            }
+            Console.WriteLine("Lets find the Deparment with Id 1");
+            var deptWithId1 = deprepo.GetDepartmentById(1);
+            Console.WriteLine($"Department with Id 1 is {deptWithId1.DeptName}");
             Console.WriteLine("Department Name");
             var deptName = Console.ReadLine();
             var newDepartment = new Department()
             {
                 DeptName = deptName
             };
+            
+            Department legalDept = new Department
+            {
+                DeptName = "Legal"
+            };
+
+            deprepo.CreateNewDepartment(legalDept);
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Added the new Legal Department!");
         }
     }
 }
